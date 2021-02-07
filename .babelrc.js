90c1f09cfa755353854fa28c36b1c78affaad515
Tim Paine <t.paine154@gmail.com>
2021-02-07 17:35:18 -0500

    .

--- .babelrc.js
+++ .babelrc.js
@@ -1,10 +1,16 @@
+const plugins = [
+  "@babel/plugin-proposal-class-properties", 
+  "@babel/plugin-proposal-object-rest-spread",
+];
+
+if (process.env.NODE_ENV === "test") {
+  plugins.push("@babel/plugin-transform-runtime");
+}
+
+
 module.exports = {
     presets: [
       "@babel/env", 
     ],    
-    plugins: [
-      "@babel/plugin-proposal-class-properties", 
-      "@babel/plugin-proposal-object-rest-spread",
-      "@babel/plugin-transform-runtime",
-    ]
+    plugins,
   };
