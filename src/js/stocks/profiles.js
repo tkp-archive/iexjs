3126d690de1350c62a29c882a863d54dfc3c02d1
Tim Paine <t.paine154@gmail.com>
2021-02-10 22:24:46 -0500

    implement profiles research and stocks

--- src/js/stocks/profiles.js
+++ src/js/stocks/profiles.js
@@ -6,3 +6,150 @@
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
+
+import { _getJson, _quoteSymbols, _raiseIfNotStr } from "../common";
+import { Client } from "../client";
+
+/**
+ * Company reference data
+ *
+ * https://iexcloud.io/docs/api/#company
+ *
+ * @param {string} symbol ticker to request
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const company = (symbol, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/company`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.company = function (symbol, filter) {
+  return company(symbol, this._token, this._version, filter);
+};
+
+/**
+ * Returns the top 10 insiders, with the most recent information.
+ *
+ * https://iexcloud.io/docs/api/#insider-roster
+ *
+ * @param {string} symbol ticker to request
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const insiderRoster = (symbol, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/insider-roster`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.insiderRoster = function (symbol, filter) {
+  return insiderRoster(symbol, this._token, this._version, filter);
+};
+
+/**
+ * Returns aggregated insiders summary data for the last 6 months.
+ *
+ * https://iexcloud.io/docs/api/#insider-summary
+ *
+ * @param {string} symbol ticker to request
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const insiderSummary = (symbol, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/insider-summary`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.insiderSummary = function (symbol, filter) {
+  return insiderSummary(symbol, this._token, this._version, filter);
+};
+
+/**
+ * Returns insider transactions.
+ *
+ * https://iexcloud.io/docs/api/#insider-transactions
+ *
+ * @param {string} symbol ticker to request
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const insiderTransactions = (symbol, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/insider-transactions`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.insiderTransactions = function (symbol, filter) {
+  return insiderTransactions(symbol, this._token, this._version, filter);
+};
+
+/**
+ * This is a helper function, but the google APIs url is standardized.
+ *
+ * https://iexcloud.io/docs/api/#logo
+ *
+ * @param {string} symbol ticker to request
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const logo = (symbol, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/logo`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.logo = function (symbol, filter) {
+  return logo(symbol, this._token, this._version, filter);
+};
+
+/**
+ * Peers of ticker
+ *
+ * https://iexcloud.io/docs/api/#peers
+ *
+ * @param {string} symbol ticker to request
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const peers = (symbol, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/peers`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.peers = function (symbol, filter) {
+  return peers(symbol, this._token, this._version, filter);
+};
--- src/js/stocks/stocks.js
+++ src/js/stocks/stocks.js
@@ -6,3 +6,71 @@
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
+
+import { _getJson, _raiseIfNotStr, _quoteSymbols, _strOrDate } from "../common";
+import { Client } from "../client";
+
+/**
+ * The following are IEX-listed securities that have an aggregate fail to deliver position for five consecutive settlement days at a registered clearing agency, totaling 10,000 shares or more and equal to at least 0.5% of the issuer’s total shares outstanding (i.e., “threshold securities”).
+ *   The report data will be published to the IEX website daily at 8:30 p.m. ET with data for that trading day.
+ *
+ * https://iexcloud.io/docs/api/#listed-regulation-sho-threshold-securities-list-in-dev
+ *
+ * @param {string} date date
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const threshold = (date, token, version, filter) => {
+  if (date) {
+    return _getJson({
+      url: `stock/market/threshold-securities/${_strOrDate(date)}`,
+      token,
+      version,
+      filter,
+    });
+  }
+  return _getJson({
+    url: `stock/market/threshold-securities`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.threshold = function (date, filter) {
+  return threshold(date, this._token, this._version, filter);
+};
+
+/**
+ * The consolidated market short interest positions in all IEX-listed securities are included in the IEX Short Interest Report.
+ *
+ * https://iexcloud.io/docs/api/#listed-short-interest-list-in-dev
+ *
+ * @param {string} symbol Ticker to request
+ * @param {string} date Effective Datetime
+ * @param {string} token Access token
+ * @param {string} version API version
+ * @param {string} filter https://iexcloud.io/docs/api/#filter-results
+ */
+export const shortInterest = (symbol, date, token, version, filter) => {
+  _raiseIfNotStr(symbol);
+  if (date) {
+    return _getJson({
+      url: `stock/${_quoteSymbols(symbol)}/short-interest/${_strOrDate(date)}`,
+      token,
+      version,
+      filter,
+    });
+  }
+  return _getJson({
+    url: `stock/${_quoteSymbols(symbol)}/short-interest`,
+    token,
+    version,
+    filter,
+  });
+};
+
+Client.prototype.shortInterest = function (symbol, date, filter) {
+  return shortInterest(symbol, date, this._token, this._version, filter);
+};
