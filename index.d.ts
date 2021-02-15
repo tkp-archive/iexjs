47471162ac333171fff971c2283db11eeaa41582
Tim Paine <t.paine154@gmail.com>
2021-02-15 16:21:26 -0500

    fix copyright

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@
  *
  * Copyright (c) 2021, the iexjs authors.
  *
- * This file is part of the jupyterlab_templates library, distributed under the terms of
+ * This file is part of the iexjs library, distributed under the terms of
  * the Apache License 2.0.  The full license can be found in the LICENSE file.
  *
  */
