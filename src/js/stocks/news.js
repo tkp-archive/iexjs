bc69d134194a8890343ef15df119750e46923781
Tim Paine <t.paine154@gmail.com>
2021-04-09 18:51:03 -0400

    fixes #34

--- src/js/stocks/news.js
+++ src/js/stocks/news.js
@@ -15,16 +15,16 @@ import { Client } from "../client";
  * https://iexcloud.io/docs/api/#news
  *
  * @param {string} symbol ticker to request
- * @param {number} count number to get
+ * @param {number} last number to get
  * @param {string} token Access token
  * @param {string} version API version
  * @param {string} filter https://iexcloud.io/docs/api/#filter-results
  * @param {string} format output format
  */
-export const news = (symbol, count, token, version, filter, format) => {
+export const news = (symbol, last, token, version, filter, format) => {
   _raiseIfNotStr(symbol);
   return _get({
-    url: `stock/${_quoteSymbols(symbol)}/news/last/${count || 10}`,
+    url: `stock/${_quoteSymbols(symbol)}/news/last/${last || 10}`,
     token,
     version,
     filter,
@@ -32,8 +32,8 @@ export const news = (symbol, count, token, version, filter, format) => {
   });
 };
 
-Client.prototype.news = function (symbol, count, filter, format) {
-  return news(symbol, count, this._token, this._version, filter, format);
+Client.prototype.news = function (symbol, last, filter, format) {
+  return news(symbol, last, this._token, this._version, filter, format);
 };
 
 /**
@@ -41,21 +41,21 @@ Client.prototype.news = function (symbol, count, filter, format) {
  *
  * https://iexcloud.io/docs/api/#news
  *
- * @param {number} count number to get
+ * @param {number} last number to get
  * @param {string} token Access token
  * @param {string} version API version
  * @param {string} filter https://iexcloud.io/docs/api/#filter-results
  * @param {string} format output format
  */
-export const marketNews = (count, token, version, filter, format) =>
+export const marketNews = (last, token, version, filter, format) =>
   _get({
-    url: `stock/market/news/last/${count || 10}`,
+    url: `stock/market/news/last/${last || 10}`,
     token,
     version,
     filter,
     format,
   });
 
-Client.prototype.marketNews = function (symbol, filter, format) {
-  return marketNews(symbol, this._token, this._version, filter, format);
+Client.prototype.marketNews = function (last, filter, format) {
+  return marketNews(last, this._token, this._version, filter, format);
 };
