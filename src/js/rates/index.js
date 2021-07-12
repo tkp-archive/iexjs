0bce6d90b1ecfa26e75d187828985b91a57ff7bf
Tim Paine <t.paine154@gmail.com>
2021-07-12 00:41:53 -0400

    more migrating

--- src/js/rates/index.js
+++ src/js/rates/index.js
@@ -7,24 +7,98 @@
  *
  */
 
-import { points } from "../points";
+import { timeSeries } from "../timeseries";
 import { Client } from "../client";
 
-export const creditcard = ({ token, version } = {}) =>
-  points({ key: "TERMCBCCALLNS" }, { token, version });
-export const cdnj = ({ token, version } = {}) =>
-  points({ key: "MMNRNJ" }, { token, version });
-export const cdj = ({ token, version } = {}) =>
-  points({ key: "MMNRJD" }, { token, version });
+/**
+ * Credit Card Interest Rate
+ * https://iexcloud.io/docs/api/#credit-card-interest-rate
+ * @param {object} timeseriesArgs
+ * @param {object} standardOptions
+ * @param {string} standardOptions.token Access token
+ * @param {string} standardOptions.version API version
+ * @param {string} standardOptions.filter https://iexcloud.io/docs/api/#filter-results
+ * @param {string} standardOptions.format output format
+ */
+export const creditcard = (
+  timeseriesArgs,
+  { token, version, format, filter } = {},
+) =>
+  timeSeries(
+    {
+      id: "RATES",
+      key: "TERMCBCCALLNS",
+      ...timeseriesArgs,
+    },
+    { token, version, format, filter },
+  );
 
-Client.prototype.creditcard = function () {
-  return creditcard({ token: this._token, version: this._version });
+/**
+ * CD Rate Non-Jumbo less than $100,000 Money market
+ * https://iexcloud.io/docs/api/#cd-rates
+ * @param {object} timeseriesArgs
+ * @param {object} standardOptions
+ * @param {string} standardOptions.token Access token
+ * @param {string} standardOptions.version API version
+ * @param {string} standardOptions.filter https://iexcloud.io/docs/api/#filter-results
+ * @param {string} standardOptions.format output format
+ */
+export const cdnj = (timeseriesArgs, { token, version, format, filter } = {}) =>
+  timeSeries(
+    {
+      id: "RATES",
+      key: "MMNRNJ",
+      ...timeseriesArgs,
+    },
+    { token, version, format, filter },
+  );
+
+/**
+ * CD Rate Jumbo more than $100,000 Money market
+ * https://iexcloud.io/docs/api/#cd-rates
+ * @param {object} timeseriesArgs
+ * @param {object} standardOptions
+ * @param {string} standardOptions.token Access token
+ * @param {string} standardOptions.version API version
+ * @param {string} standardOptions.filter https://iexcloud.io/docs/api/#filter-results
+ * @param {string} standardOptions.format output format
+ */
+export const cdj = (timeseriesArgs, { token, version, format, filter } = {}) =>
+  timeSeries(
+    {
+      id: "RATES",
+      key: "MMNRJD",
+      ...timeseriesArgs,
+    },
+    { token, version, format, filter },
+  );
+
+Client.prototype.creditcard = function (
+  timeseriesArgs,
+  { filter, format } = {},
+) {
+  return creditcard(timeseriesArgs, {
+    token: this._token,
+    version: this._version,
+    filter,
+    format,
+  });
 };
 
-Client.prototype.cdnj = function () {
-  return cdnj({ token: this._token, version: this._version });
+Client.prototype.cdnj = function (timeseriesArgs, { filter, format } = {}) {
+  return cdnj(timeseriesArgs, {
+    token: this._token,
+    version: this._version,
+    filter,
+    format,
+  });
 };
 
-Client.prototype.cdj = function () {
-  return cdj({ token: this._token, version: this._version });
+Client.prototype.cdj = function (timeseriesArgs, { filter, format } = {}) {
+  return cdj(timeseriesArgs, {
+    token: this._token,
+    version: this._version,
+    filter,
+    format,
+  });
 };
